96f04cfd64af97080f13925ad9cdd12ddacc9c63
Stephan Sutter <stephan.sutter2000@gmail.com>
2019-07-17 18:51:21 +0800

    Added drop off location setting

--- app/src/main/java/com/example/vanaid/fragments/HomeFragment.java
+++ app/src/main/java/com/example/vanaid/fragments/HomeFragment.java
@@ -9,10 +9,12 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,15 +30,22 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.LocationRestriction;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeolocationApi;
+import com.google.maps.RoadsApi;
+import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.SnappedPoint;
 import com.google.maps.model.TravelMode;
 
 import org.joda.time.DateTime;
@@ -56,7 +65,7 @@ import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraIdleListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -71,6 +80,7 @@ public class HomeFragment extends Fragment {
     private Boolean mLocationPermissionGranted = false;
     private Location mLastKnownLocation;
     private GoogleMap googleMap;
+    private Marker pickupMarker;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -116,13 +126,36 @@ public class HomeFragment extends Fragment {
 
         // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(
+                new LatLng(15.932911, 119.944304),
+                new LatLng(18.342771, 121.046823)
+        ));
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                try {
+                    Geocoder geocoder;
+                    List<Address> addresses;
+                    geocoder = new Geocoder(getActivity(), Locale.getDefault());
+
+                    addresses = geocoder.getFromLocationName(place.getName(), 1);
+
+                    com.google.maps.model.LatLng center = new com.google.maps.model.LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
+                    SnappedPoint[] points = RoadsApi.snapToRoads(getGeoContext(), true, center).await();
+                    if(points != null){
+                        pickupMarker.setPosition(new LatLng(points[0].location.lat, points[0].location.lng));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(points[0].location.lat, points[0].location.lng), 20));
+                    }
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -159,6 +192,27 @@ public class HomeFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onCameraMove() {
+        pickupMarker.setPosition(googleMap.getCameraPosition().target);
+    }
+
+    @Override
+    public void onCameraIdle() {
+        try {
+            SnappedPoint[] points = RoadsApi.snapToRoads(getGeoContext(), true, new com.google.maps.model.LatLng(googleMap.getCameraPosition().target.latitude, googleMap.getCameraPosition().target.longitude)).await();
+            if(points != null){
+                pickupMarker.setPosition(new LatLng(points[0].location.lat, points[0].location.lng));
+            }
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -183,35 +237,40 @@ public class HomeFragment extends Fragment {
 
     private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                googleMap = mMap;
+        mapFragment.getMapAsync(this);
+    }
 
-                LatLng center = new LatLng(17.127995,120.485980);
-                if(mLastKnownLocation != null){
-                    center = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
-                }
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap = mMap;
 
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 10));
+        LatLng center = new LatLng(17.127995,120.485980);
+        if(mLastKnownLocation != null){
+            center = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+        }
 
-                if (ContextCompat.checkSelfPermission(getActivity(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED
-                        && ContextCompat.checkSelfPermission(getActivity(),
-                        Manifest.permission.ACCESS_FINE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 10));
 
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
-                    return;
-                } else {
-                    mLocationPermissionGranted = true;
-                }
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-                if (mLocationPermissionGranted) {
-                    updateMap();
-                }
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
+            return;
+        } else {
+            mLocationPermissionGranted = true;
+        }
+
+        if (mLocationPermissionGranted) {
+            updateMap();
+        }
+
+        googleMap.setOnCameraMoveListener(this);
+        googleMap.setOnCameraIdleListener(this);
 //                LatLng orig = new LatLng(16.5542868,120.3232215);
 //                LatLng dest = new LatLng(16.6169121,120.31527);
 //                String o = orig.latitude + "," +  orig.longitude;
@@ -250,8 +309,6 @@ public class HomeFragment extends Fragment {
 //                } catch (com.google.maps.errors.ApiException e) {
 //                    e.printStackTrace();
 //                }
-            }
-        });
     }
 
     @Override
@@ -272,7 +329,18 @@ public class HomeFragment extends Fragment {
     }
 
     public void setMapCenter(){
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), 10));
+        try {
+            SnappedPoint[] points = RoadsApi.snapToRoads(getGeoContext(), true, new com.google.maps.model.LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude())).await();
+            pickupMarker = googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(points[0].location.lat, points[0].location.lng)));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(points[0].location.lat, points[0].location.lng), 15));
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void updateMap(){
