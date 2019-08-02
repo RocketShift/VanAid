1caeb0b06d67f8a692a7e6dada1af399c18c5f11
Stephan Sutter <stephan.sutter2000@gmail.com>
2019-08-02 12:08:23 +0800

    Added firebase cloud messaging

---
+++ app/src/main/java/com/example/vanaid/services/MessagingService.java
@@ -0,0 +1,10 @@
+package com.example.vanaid.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
+}
